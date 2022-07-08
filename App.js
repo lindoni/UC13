4ebe72c9bc356ab1490284a4087f6cd199bfb28a
lindoni <camila.lindoni@gmail.com>
2022-07-08 11:12:03 -0300

    melhorias realizadas

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
   // Mapeamento de teclas
-  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9, "x", 6, 5, 4, '-', 3, 2, 1, '+', 0, '.', '+/-', '=']
+  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9, "*", 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '+/-', '=']
 
   const [currentNumber, setCurrentNumber] = useState("")
   const [lastNumber, setLastNumber] = useState("")
@@ -42,7 +42,7 @@ export default function App() {
     }
     switch (buttonPressed) {
       case 'DEL':
-        setCurrentNumber(currentNumber.substring(0, (currentNumber.length - 2)))
+        setCurrentNumber(currentNumber.substring(0, (currentNumber.length - 1)))
         return
       case 'LIMPAR': // Limpa todo o conteúdo
         setLastNumber("")
@@ -74,12 +74,12 @@ export default function App() {
 
         {buttons.map((button) =>
           button === '=' ? // Mapeamento do botão =
-            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#3dd0e3' }]}>
+            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#4b0082' }]}>
               <Text style={[styles.textButton, { color: "white", fontSize: 30 }]}>{button}</Text>
             </TouchableOpacity>
             : // Mapeamento dos outros botões
             <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-              <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'black' : '#0093a6' }]}>{button}</Text>
+              <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'white' : 'gray' }]}>{button}</Text>
             </TouchableOpacity>
         )}
       </View>
@@ -97,17 +97,17 @@ const styles = StyleSheet.create({
   results: {
         flex: 2,
         justifyContent: "center",
-        backgroundColor: "#f5f5f5"
+        backgroundColor: "#4b0082"
   },
   resultText: {
-        color: "#282F38",
+        color: "white",
         fontSize: 32,
         fontWeight: "bold",
         padding: 12,
         textAlign: "right"
       },
       historyText: {
-        color: "#7c7c7c",
+        color: "gray",
         fontSize: 20,
         marginRight: 10,
         alignSelf: 'flex-end',
@@ -117,7 +117,7 @@ const styles = StyleSheet.create({
         flexWrap: 'wrap',
       },
       button: {
-        backgroundColor: 'white',
+        backgroundColor: '#663399',
         alignItems: 'center',
         justifyContent: 'center',
         minWidth: 90,
@@ -125,7 +125,7 @@ const styles = StyleSheet.create({
         flex: 2,
       },
       textButton: {
-        color: "#7c7c7c",
+        color: "white",
         fontSize: 20,
       }
 })
